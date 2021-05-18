a1d45e50833f25c882b6ea1192fc6a906171eddb
Aleksandr Kunin <skyksandr@gmail.com>
2021-05-18 10:57:09 +0300

    Update dev dependencies

--- test/index.js
+++ test/index.js
@@ -1,17 +1,16 @@
 import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
-import { transformFileSync } from 'babel-core';
-import plugin from '../src';
+import { transformFileSync } from '@babel/core';
 
 function trim(str) {
   return str.replace(/^\s+|\s+$/, '');
 }
 
-describe('', () => {
+describe('plugin', () => {
   const fixturesDir = path.join(__dirname, 'fixtures');
   fs.readdirSync(fixturesDir).map((caseName) => {
-    it(`should ${caseName.split('-').join(' ')}`, () => {
+    it(`should transform ${caseName.split('-').join(' ')}`, () => {
       const fixtureDir = path.join(fixturesDir, caseName);
       const actualPath = path.join(fixtureDir, 'actual.js');
       const actual = transformFileSync(actualPath).code;
